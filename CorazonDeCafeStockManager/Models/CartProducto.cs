2b871cb409581517c7fb1bd8fc162d69ce085e2f
Johan Guevara <johanguevara1221@gmail.com>
2023-08-25 15:43:06 -0300

    ADD Entity Relations

---
+++ CorazonDeCafeStockManager/Models/CartProducto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CorazonDeCafeStockManager.Models;
+
+public partial class CartProducto
+{
+    public int Id { get; set; }
+
+    public int? CartId { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public decimal? Precio { get; set; }
+
+    public int? Cantidad { get; set; }
+
+    public virtual Cart? Cart { get; set; }
+
+    public virtual Product? Product { get; set; }
+}
---
+++ CorazonDeCafeStockManager/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CorazonDeCafeStockManager.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public float Precio { get; set; }
+
+    public string Imagen { get; set; } = null!;
+
+    public int CategoriaId { get; set; }
+
+    public int TipoId { get; set; }
+
+    public string Estado { get; set; } = null!;
+
+    public int? Stock { get; set; }
+
+    public virtual ICollection<CartProducto> CartProductos { get; set; } = new List<CartProducto>();
+
+    public virtual Categoria Categoria { get; set; } = null!;
+
+    public virtual Tipo Tipo { get; set; } = null!;
+}
