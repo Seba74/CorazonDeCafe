b4ec89d033a654a765bc6d74d7a811d581a34ea8
Johan Guevara <johanguevara1221@gmail.com>
2023-08-29 00:02:59 -0300

    Fixed trouble

--- CorazonDeCafeStockManager/app/Views/Products-Form/Products.cs
+++ CorazonDeCafeStockManager/app/Views/Products-Form/Products.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using CorazonDeCafeStockManager.App.Common;
 using CorazonDeCafeStockManager.App.Models;
 using CorazonDeCafeStockManager.App.Views.Login_Form;
 
